19ffea788cc27be4640d61fa153286e7996c58de
tristanfisher <code@tristanfisher.com>
2013-10-02 18:44:29 -0400

    don't catch general try/except errors...

--- check_http_response.py
+++ check_http_response.py
@@ -5,7 +5,7 @@ import requests
 import sys
 import subprocess
 
-VERSION='1.00'
+VERSION='1.01'
 def show_version(): print(VERSION)
 
 class Nagios:
@@ -16,92 +16,55 @@ class Nagios:
 
 nagios = Nagios()
 
-def s(x):
-    """return a string encoded equivalent of input.  paranoid check meant as a quick decorator."""
-    return str(x)
-
 def nagiosExit(exit_code,msg=None):
-    """Exit script with a str() message and an integer 'nagios_code', which is a sys.exit level.
-
-    Named nagiosExit to avoid confusion with sys.exit
-    """
+    """Exit script with a str() message and an integer 'nagios_code', which is a sys.exit level."""
     if (msg == None):
         sys.exit(nagios.ok[0])
     else:
         print(exit_code[0],exit_code[1] + " - " + str(msg))
         sys.exit(exit_code[0])
 
-#TODO: generalize a check_response function and only then call a function to compare the data
-# def get_uri_response(uri); def_check_json() or def_check_content()
-
-def check_text_response(uri, expected_response_data):
-    """ Check URI for any (even binary data) content."""
-
-    def checkMatch(data_response, expected_response_data):
-        """ Simply do an equality comparison between two objects.  Caution, may do 'hilarious' things on binary data. """
-        try:
-            if data_response != expected_response_data:
-                returnValue = "{0} :: expected {1} received {2}".format(data_response, expected_response_data)
-                nagiosExit(nagios.critical, str(returnValue))
-            else:
-                nagiosExit(nagios.ok)
-        except:
-            nagiosExit(nagios.critical, str("{0} :: Failed to grab/parse data {1}").format(uri, data_response))
+def uri_request(uri, expected_response_format=None, arg0=None, arg1=None):
     try:
         request = requests.get(uri)
         if request.status_code > 200:
-            nagiosExit(nagios.critical,str("Status code: {0} :: {1}".format(request.status_code, uri)))
+            nagiosExit(nagios.critical, str("Status code: {0} :: {1}".format(request.status_code, uri)))
+
         else:
-            data_response = request.content
-            checkMatch(data_response, expected_response_data)
+            if expected_response_format == 'JSON': 
+                json_response = json.loads(request.text)
+                check_json_response(json_response, check_json_key=arg0, check_json_value=arg1)
+            else: 
+                text_response = str(request.content) #be careful on strings that could look like other types
+                check_text_response(text_response, arg0) 
 
-    except requests.ConnectionError:
-        nagiosExit(nagios.critical,str("Error loading :: {0}".format(uri)))
-    except requests.Timeout:
-        nagiosExit(nagios.critical,str("Timed out :: {0}".format(uri)))
-    except requests.HTTPError:
-        nagiosExit(nagios.critical,str("Invalid HTTP response :: {0}".format(uri)))
-    except requests.TooManyRedirects:
-        nagiosExit(nagios.critical,str("Too many redirects :: {0}".format(uri)))
+    except requests.ConnectionError : nagiosExit(nagios.critical,str("Error loading :: {0}".format(uri)))
+    except requests.Timeout : nagiosExit(nagios.critical,str("Timed out :: {0}".format(uri)))
+    except requests.HTTPError : nagiosExit(nagios.critical,str("Invalid HTTP response :: {0}".format(uri)))
+    except requests.TooManyRedirects: nagiosExit(nagios.critical,str("Too many redirects :: {0}".format(uri)))
 
-    if request.text != expected_response_data:
-        nagiosExit(nagios.critical,str("{0} :: "))
-    else:
-        nagiosExit(nagios.ok)
-
-def check_json_response(uri, response_key, expected_response_key_data):
-    """ check a json key/value pair against a HTTP URI.
+def check_json_response(json_response,check_json_key,check_json_value):
+    """ check a json key/value pair against a HTTP response.
     match returns nagiosExit(0), otherwise nagiosExit(1) is returned.
     """
-
-    def checkMatch(json_response, response_key, expected_response_key_data):
-        try:
-            if json_response[response_key] != expected_response_key_data:
-                returnValue = "{0} :: expected '{1}:{2}' received '{3}'".format(uri, response_key, expected_response_key_data, json_response[response_key])
-                nagiosExit(nagios.critical, str(returnValue))
-            else:
-                nagiosExit(nagios.ok)
-        except KeyError:
-            nagiosExit(nagios.critical, str("{0} :: Failed to find JSON key: {1}").format(uri, response_key))
-
     try:
-        request = requests.get(uri)
-        if request.status_code > 200:
-            nagiosExit(nagios.critical,str("Status code: {0} :: {1}".format(request.status_code, uri)))
+        if json_response[check_json_key] != check_json_value:
+            returnValue = "Expected '{0}:{1}' ; Received '{2}'".format(check_json_key, check_json_value, json_response[check_json_key])
+            nagiosExit(nagios.critical, str(returnValue))
         else:
-            json_response = json.loads(request.text)
-            checkMatch(json_response, response_key, expected_response_key_data)
+            nagiosExit(nagios.ok)
+    except KeyError:
+        ''' key could not be looked up in the response data from the URI '''
+        nagiosExit(nagios.critical, str("KeyError :: Failed to find JSON key: {0}").format(check_json_value))
 
-    #consider using unknown, depending on context.  nagios.critical is a sane default because an unknown
-    #status is often just as bad as an erroneous state..
-    except requests.ConnectionError:
-        nagiosExit(nagios.critical,str("Error loading :: {0}".format(uri)))
-    except requests.Timeout:
-        nagiosExit(nagios.critical,str("Timed out :: {0}".format(uri)))
-    except requests.HTTPError:
-        nagiosExit(nagios.critical,str("Invalid HTTP response :: {0}".format(uri)))
-    except requests.TooManyRedirects:
-        nagiosExit(nagios.critical,str("Too many redirects :: {0}".format(uri)))
+
+def check_text_response(text_response,expected_response_text):
+    """ Simply do an equality comparison between two objects.  Caution, may do 'hilarious' things on binary data. """
+    if str(text_response) != str(expected_response_text):
+        returnValue = "Expected {0} ; Received: {1}".format(expected_response_text, text_response)
+        nagiosExit(nagios.critical,str(returnValue))
+    else:
+        nagiosExit(nagios.ok)
 
 import argparse
 parser = argparse.ArgumentParser(description=str("A Nagios-ready python script for comparing data retrieved from an HTTP source.\n ./check_http_response.py --host 'https://example.org/status' --json 'status' 'ok'"))
@@ -117,9 +80,11 @@ args = parser.parse_args()
 if args.version:
     show_version()
 if args.json:
-    check_json_response(args.server, args.json[0], args.json[1])
+    uri_request(uri=args.server, expected_response_format='JSON', arg0=args.json[0], arg1=args.json[1])
+    #args.json[0], args.json[1])
 if args.text:
-    check_text_response(args.server, args.text)
+    uri_request(uri=args.server, expected_response_format='text', arg0=str(args.text[0]))
+    #args.text)
 
 def test():
     check_json_response('https://example.org/status', 'status', 'ok')
@@ -127,4 +92,3 @@ def test():
 if __name__ == "__main__":
   subprocess.call(["/usr/bin/env","python","check_http_response.py","-h"])
   sys.exit(1)
-
